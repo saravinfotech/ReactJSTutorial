a73280f14fcee37109b3551cfa4013f37683c3ca
Saravanan <saravinfotech@gmail.com>
2019-09-08 02:49:34 +0100

    Seggregating the component into separate files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Intro from './Intro';
+
 
 class App extends Component{
   render(){
@@ -13,24 +16,4 @@ class App extends Component{
   }
 }
 
-class Header extends Component{
-  render(){
-    return(
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <h2> Welcome to React</h2>
-        </div>
-    );
-  }
-}
-
-class Intro extends Component{
-  render(){
-    return(
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    );
-  }
-}
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+class Header extends Component{
+  render(){
+    return(
+        <div className="App-header">
+        {
+        //  <img src={logo} className="App-logo" alt="logo" />
+        }
+            <h2> Welcome to React</h2>
+        </div>
+    );
+  }
+}
+
+export default Header;
