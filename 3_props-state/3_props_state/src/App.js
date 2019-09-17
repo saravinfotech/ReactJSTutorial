22e6a696f7b8a4ee48168faa9c52fc5332f5a2a7
154773 <saravanan6.kumar6@cognizant.com>
2019-09-17 15:42:23 +0100

    Property types

--- 3_props-state/3_props_state/src/App.js
+++ 3_props-state/3_props_state/src/App.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.name ="hello";
+  }
+
+  sampleFunction(calledFrom){
+    return "This method is "+calledFrom +" reactJS";
+  }
+
+  render(){
+    const name="Sarav";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Parent />
-      </header>
+      <p> Welcome {name} this is keyword access sample</p>
+
+      <p>This is a sample of method access to print. {this.sampleFunction("called from")}</p>
+
+    <p> This is a sample to demo the call of constructor {this.name}</p>
+    <h3>prop numbers is :{this.props.propNumbers}</h3>
+    <h3>prop String is :{this.props.propString}</h3>
+    <h3> prop Object is :{this.props.propObject.obj1}</h3>
+    <Parent />
     </div>
   );
+}
+}
+
+App.propTypes = {
+  propObject : PropTypes.object,
+  propString : PropTypes.string,
+  propNumbers : PropTypes.number
+}
+
+App.defaultProps = {
+  propNumbers : 3,
+  propString : "Hello World",
+  propObject : {
+    obj1 : "I am first object",
+    obj2 : "I am second object",
+    obj3 : "I am third object",
+  }
 }
 
 class Parent extends React.Component{
@@ -33,6 +58,7 @@ class Parent extends React.Component{
     );
   }
 }
+
 
 Parent.defaultProps = {
   cars:["BMW","Audi","Merc","City"]
