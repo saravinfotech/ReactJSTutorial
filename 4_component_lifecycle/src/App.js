452642584f2687a42919b8413bb6c6d8201dc2c0
154773 <saravanan6.kumar6@cognizant.com>
2019-09-18 01:07:47 +0100

    Basic state setup for lifecycles

--- 4_component_lifecycle/src/App.js
+++ 4_component_lifecycle/src/App.js
@@ -6,21 +6,48 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" height="100px" width="100px" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Body />
       </header>
     </div>
   );
 }
 
+class Body extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      randomNumber : 0
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+
+  getRandomNumber(){
+    //alert("Hello")
+    this.setState({randomNumber:Math.floor(Math.random()*10)});
+  }
+
+  render(){
+    return (
+      <div> <h2>This is a sample text</h2>
+      <button onClick={this.getRandomNumber}> Random Number Generator</button>
+      <Numbers myNumber = {this.state.randomNumber}/>
+  </div>
+    );
+  }
+}
+
+class Numbers extends React.Component{
+  render(){
+    return(
+      <div>
+        <br/>
+        {this.props.myNumber}
+      </div>
+    );
+  }
+}
 export default App;
